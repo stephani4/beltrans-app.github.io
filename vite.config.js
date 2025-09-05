c4c340d34e29d5bd6ed85e550ee1718fdff8edd2
s.gubin <s.gubin@stroyservis.com>
2025-09-05 10:53:52 +0700

    Доработка по верстке

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ export default (mode) => {
     return defineConfig({
         base: 'beltrans-app.github.io',
         // ...(mode === 'production' ? {} : {base: 'beltrans-app.github.io'}),
-        build: {
-            outDir: './dist/beltrans-app.github.io'
-        }
+        // build: {
+        //     outDir: './dist/beltrans-app.github.io'
+        // }
     })
 }
